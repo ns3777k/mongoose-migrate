6174207f3181235b917650c513349e03666936f9
Nikita Safonov <ns3777k@gmail.com>
2019-04-21 17:04:35 +0300

    testing travis

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo $TRAVIS_TAG
+IMAGE="ns3777k/mongoose-migrate:${TRAVIS_TAG}"
+echo $IMAGE
 
 #echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker push ns3777k/mongoose-migrate:"$TRAVIS_TAG"
+#docker push
