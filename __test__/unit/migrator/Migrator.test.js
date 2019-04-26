fd5ea96d661621cc600177340490f512f6716fab
Nikita Safonov <ns3777k@gmail.com>
2019-04-26 23:34:42 +0300

    add more migrator tests

--- __test__/unit/migrator/Migrator.test.js
+++ __test__/unit/migrator/Migrator.test.js
@@ -9,6 +9,8 @@ describe('Migrator', () => {
   beforeEach(() => {
     FileStorage.mockClear();
     DatabaseStorage.mockClear();
+
+    delete global.__non_webpack_require__;
   });
 
   it('splits migrations into pending and applied', async () => {
@@ -46,5 +48,55 @@ describe('Migrator', () => {
       '1554994809710-channel',
       '1555994809710-regions'
     ]);
+  });
+
+  it('applies migration', async () => {
+    const mock = { up: jest.fn() };
+    global.__non_webpack_require__ = jest.fn().mockReturnValueOnce(mock);
+
+    FileStorage.mockImplementation(() => {
+      return {
+        locateMigration: () => '/1554334809710-migration.js'
+      };
+    });
+
+    DatabaseStorage.mockImplementation(() => {
+      return {
+        getClient: () => null,
+        applyMigration: async () => true
+      };
+    });
+
+    const fs = new FileStorage('/');
+    const db = new DatabaseStorage('');
+    const migrator = new Migrator(fs, db);
+    const result = await migrator.applyMigration('1554334809710-migration');
+    expect(result).toStrictEqual(true);
+    expect(mock.up).toHaveBeenNthCalledWith(1, null);
+  });
+
+  it('rollbacks migration', async () => {
+    const mock = { down: jest.fn() };
+    global.__non_webpack_require__ = jest.fn().mockReturnValueOnce(mock);
+
+    FileStorage.mockImplementation(() => {
+      return {
+        locateMigration: () => '/1554334809710-migration.js'
+      };
+    });
+
+    DatabaseStorage.mockImplementation(() => {
+      return {
+        getClient: () => null,
+        rollbackMigration: async () => true
+      };
+    });
+
+    const fs = new FileStorage('/');
+    const db = new DatabaseStorage('');
+    const migrator = new Migrator(fs, db);
+    const result = await migrator.rollbackMigration('1554334809710-migration');
+    expect(result).toStrictEqual(true);
+    expect(mock.down).toHaveBeenNthCalledWith(1, null);
   });
 });
